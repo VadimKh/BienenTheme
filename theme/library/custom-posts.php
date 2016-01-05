340ffc2d8c6840fe52aff3d3d50ac6abf6eaa422
Vadim <12h@bk.ru>
2016-01-05 13:50:04 +0300

    Create custom products with custom category

--- theme/library/custom-posts.php
+++ theme/library/custom-posts.php
@@ -14,41 +14,52 @@ function bienen_remove_posts_menu() {
 }
 add_action('admin_menu', 'bienen_remove_posts_menu');
 
-function bienen_create_post_type_example()
+function bienen_create_post_type_products()
 {
-    register_taxonomy_for_object_type('category', 'example'); // Register Taxonomies for Category
-    register_taxonomy_for_object_type('post_tag', 'example');
-    register_post_type('example', // Register Custom Post Type
+    //register_taxonomy_for_object_type('category', 'products'); // Register Taxonomies for Category
+    //register_taxonomy_for_object_type('post_tag', 'products');
+    register_post_type('products', // Register Custom Post Type
         array(
             'labels' => array(
-                'name' => __('HTML5 Blank Custom Post', 'bienen'), // Rename these to suit
-                'singular_name' => __('HTML5 Blank Custom Post', 'bienen'),
+                'name' => __('Products', 'bienen'), // Rename these to suit
+                'singular_name' => __('Product', 'bienen'),
                 'add_new' => __('Add New', 'bienen'),
-                'add_new_item' => __('Add New HTML5 Blank Custom Post', 'bienen'),
+                'add_new_item' => __('Add New Product', 'bienen'),
                 'edit' => __('Edit', 'bienen'),
-                'edit_item' => __('Edit HTML5 Blank Custom Post', 'bienen'),
-                'new_item' => __('New HTML5 Blank Custom Post', 'bienen'),
-                'view' => __('View HTML5 Blank Custom Post', 'bienen'),
-                'view_item' => __('View HTML5 Blank Custom Post', 'bienen'),
-                'search_items' => __('Search HTML5 Blank Custom Post', 'bienen'),
-                'not_found' => __('No HTML5 Blank Custom Posts found', 'bienen'),
-                'not_found_in_trash' => __('No HTML5 Blank Custom Posts found in Trash', 'bienen')
+                'edit_item' => __('Edit Product', 'bienen'),
+                'new_item' => __('New Product', 'bienen'),
+                'view' => __('View Product', 'bienen'),
+                'view_item' => __('View Product', 'bienen'),
+                'search_items' => __('Search Products', 'bienen'),
+                'not_found' => __('Products not found', 'bienen'),
+                'not_found_in_trash' => __('Products not found in Trash', 'bienen')
             ),
             'public' => true,
             'hierarchical' => true, // Allows your posts to behave like Hierarchy Pages
-            'has_archive' => true,
             'supports' => array(
-                'title',
-                'editor',
-                'excerpt',
-                'thumbnail'
-            ), // Go to Dashboard Custom HTML5 Blank post for supports
+                'title', 'editor', 'author', 'page-attributes'
+            ),
             'can_export' => true, // Allows export in Tools > Export
-            'taxonomies' => array(
-                'post_tag',
-                'category'
-            ) // Add Category and Post Tags support
         ));
+
+    register_taxonomy('products_category', array('products'), array(
+        'label'                 => '', // определяется параметром $labels->name
+        'labels'                => array(
+            'name'              => __('Categories', 'bienen'),
+            'singular_name'     => __('Category', 'bienen'),
+            'search_items'      => __('Search Categories', 'bienen'),
+            'all_items'         => __('All Categories', 'bienen'),
+            'parent_item'       => __('Parent Category', 'bienen'),
+            'parent_item_colon' => __('Parent Category:', 'bienen'),
+            'edit_item'         => __('Edit Category', 'bienen'),
+            'update_item'       => __('Update Category', 'bienen'),
+            'add_new_item'      => __('Add New Category', 'bienen'),
+            'new_item_name'     => __('New Category Name', 'bienen'),
+            'menu_name'         => __('Category', 'bienen'),
+        ),
+        'public'                => true,
+        'hierarchical'          => true,
+    ) );
 }
 
-add_action('init', 'bienen_create_post_type_example'); // Add our HTML5 Blank Custom Post Type
+add_action('init', 'bienen_create_post_type_products'); // Add our HTML5 Blank Custom Post Type
